680d886ce50f9db5a03a04864a7271ea02f36bae
Will Auman <willauman2@gmail.com>
2024-06-25 10:38:48 -0700

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+input("Welcome to the addition calculator! Would you like to start?")
+start = True
+if start:
+  num1 = int(input("Choose a number"))
+  num2 = int(input("Choose another number"))
+
+print(" The sum of the two numbers is" ,num1+num2)
+
